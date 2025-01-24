999d22b16eb385d753b1e1ea6f207aa8c7a8f016
digipols-admin <admin@digipols.com>
2025-01-24 20:39:21 +0000

    updated static2

--- R/scrap.R
+++ R/scrap.R
@@ -8,3 +8,8 @@ destination_dir <- "~/fm_teaching/teaching_template/surveys-tab"
 
 # Copy and rename the directory
 dir_copy(source_dir, destination_dir)
+
+# Terminal scripts
+# quarto render
+# git add . && git commit -m "Add git steps2" && git push
+# git config --global credential.helper cache
